cf76bd615861d189af918c1e204d39788082f5f1
brendandegryse-92 <62355127+brendandegryse-92@users.noreply.github.com>
2020-03-22 16:26:34 -0400

    3/22/20

--- index.php
+++ index.php
@@ -11,39 +11,61 @@
   </style>
 </head>
 <body>
-<h1><a href="index.php" class="head">Simplified Technology Services Inc.</a></h1>
-<div class="nav">
-  <a href="operators.php">Operators</a>
-  <a href="farms.php">Farms</a>
-  <a href="fields.php">Fields</a>
-  <a href="prices.php">Prices</a>
-  <a href="crop.php">Crop</a>
-  <a href="applicants.php">Applications</a>
-  <a href="chemicalsForm.php">Chemicals</a>
-  <a href="#Feritalizers">Feritalizers</a>
-  <a href="forms.php">Forms</a>
+  <div include="head.html"></div>
   <?php
   session_start();
   $server = "localhost";
   $uname = "client";
   $pword = "Pass";
-try {
+  try {
   $connection = new PDO("mysql:host=$server;dbname=simplifiedtechnologyservices",$uname,$pword);
   $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::FETCH_ASSOC);
-}
-catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();}
-if ($_SESSION['ID'] == null) {
+  }
+  catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();}
+  echo '<div class="nav">';
+  if ($_SESSION['ID'] == null) {
   echo '<a href="login.php" class="Login">Login</a>';
+  }
+  else {
+  $sql = "SELECT name FROM users WHERE UserID = ?";
+  $stmt = $connection->prepare($sql);
+  $stmt->execute([$_SESSION['ID']]);
+  $name = $stmt->fetch();
+  echo '<a href="account.php">'.$name[0].'</a>';
 }
-else {
-$sql = "SELECT name FROM users WHERE UserID = ?";
-$stmt = $connection->prepare($sql);
-$stmt->execute([$_SESSION['ID']]);
-$name = $stmt->fetch();
-echo '<a href="account.php">'.$name[0].'</a>';
-}
+echo '</div>';
   ?>
-  </div>
-  <p>Welcome to On Farm Record Keeping</p>
+  <p>Welcome to On Farm Record Keeping</p><script>
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;alert(this.responseText);}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("include");
+          includeHTML();
+        }
+      }
+      xhttp.open("POST", file, false);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+</script>
+<script>
+includeHTML();
+</script>
 </body>
 </html>

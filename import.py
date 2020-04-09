d4a07ce525cad89774deffbe66006ed0824edc05
brendandegryse-92 <62355127+brendandegryse-92@users.noreply.github.com>
2020-04-08 21:12:36 -0400

    4/8/20

--- import.py
+++ import.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import mysql.connector
-file = pd.read_csv('C:\Users\brend\Downloads\Roemke-ApplicationReportData from MyJohnDeere3Apps.csv')
-file.head()
+file = pd.read_csv('C:/Users/brend/Downloads/operator.csv')
+#print(file['OpName'])
+values = [
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="client",
+  passwd="Pass",
+  database="simplifiedtechnologyservices"
+)
+
+cursor = mydb.cursor()
+
+sql = "INSERT INTO operator (OpName, OpAddress, OpCity, OpState, OpZip, OpPhone, IsActive, UserID) Values (%s,%s,%s,%s,%s,%s,%s,%s)"
+for i in range(len(file)):
+    print((file['OpName'][i], file['OpAddress'][i], file['OpCity'][i], file['OpState'][i], file['OpZip'][i], file['OpPhone'][i], file['IsActive'][i], 22))
+    values = values.append((file['OpName'][i], file['OpAddress'][i], file['OpCity'][i], file['OpState'][i], file['OpZip'][i], file['OpPhone'][i], file['IsActive'][i], 22))
+print(values)
+cursor.execute(sql, values)

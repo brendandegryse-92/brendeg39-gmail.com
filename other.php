afe180b9fe2d8b19c5c4f71bf21f9edb93e256cc
brendandegryse-92 <62355127+brendandegryse-92@users.noreply.github.com>
2020-05-12 10:07:01 -0400

    5/12/20

--- other.php
+++ other.php
@@ -2,18 +2,19 @@
 <head>
 </head>
 <body>
+  <a href = "#field.php">Field</a>
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    First Name:<input type="text"></input>
-    Middle Initial:<input type="text"></input>
-    Last Name:<input type="text"></input>
-    Company Name:<input type="text"></input>
-    Mailing Address:<input type="text"></input>
-    City:<input type="text"></input>
-    State:<input type="text"></input>
-    ZIP:<input type="number"></input>
-    Home Phone:<input type="phone"></input>
-    Mobile Phone:<input type="phone"></input>
-    Email:<input type="email"></input>
+    First Name:<input type="text" name="FirstName"></input>
+    Middle Initial:<input type="text" name="MI"></input>
+    Last Name:<input type="text"name="LastName"></input>
+    Company Name:<input type="text" name="CompanyName"></input>
+    Mailing Address:<input type="text" name="MailAdd"></input>
+    City:<input type="text" name="City"></input>
+    State:<input type="text" name="State"></input>
+    ZIP:<input type="number" name="ZIP"></input>
+    Home Phone:<input type="phone" name="Home"></input>
+    Mobile Phone:<input type="phone" name="Mobile"></input>
+    Email:<input type="email" name="Email"></input>
     <input type="submit"></input>
   </form>
 <?php
@@ -26,6 +27,24 @@ $connection = new PDO("mysql:host=$server;dbname=fieldreports",$uname,$pword);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::FETCH_ASSOC);
 }
 catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();
+}
+$sql = 'SELECT * FROM grower';
+$stmt = $connection->prepare($sql);
+$stmt->execute();
+$arr = $stmt->fetchAll(PDO::FETCH_NUM);
+foreach ($arr as $i=>$val) {
+  array_push($_SESSION['rowPrimaryID'], $val[0]);
+  foreach ($val as $key => $value) {
+    echo $value;
+  }
+  echo '<br>';
+  echo 'alert';
+  if (isset($_POST['FirstName'])) {
+  echo '<script>alert("sdlf");</script>';
+  $sql = 'INSERT INTO grower (FirstName, MI, LastName, CompanyName, MailingAddress, City, State, Zip, HomePhone, MobilePhone, Email) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
+  $stmt = $connection->prepare($sql);
+  $stmt->execute([$_POST['FirstName'], $_POST['MI'], $_POST['LastName'], $_POST['CompanyName'], $_POST['MailAdd'], $_POST['City'], $_POST['State'], $_POST['ZIP'], $_POST['Home'], $_POST['Mobile'], $_POST['Email']]);
+  }
 }
 ?>
 </body>

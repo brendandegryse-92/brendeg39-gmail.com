0669cc189275cbf42de61d4fd0cb37084547673b
brendandegryse-92 <62355127+brendandegryse-92@users.noreply.github.com>
2020-05-15 15:03:22 -0400

    5/15/20

--- otherfield.php
+++ otherfield.php
@@ -2,19 +2,27 @@
 <head>
 </head>
 <body>
-  <a href="otherfield.php">Field</a>
+  <a href="other.php">Grower</a>
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    First Name:<input type="text" name="FirstName"></input>
-    Middle Initial:<input type="text" name="MI"></input>
-    Last Name:<input type="text"name="LastName"></input>
-    Company Name:<input type="text" name="CompanyName"></input>
-    Mailing Address:<input type="text" name="MailAdd"></input>
-    City:<input type="text" name="City"></input>
-    State:<input type="text" name="State"></input>
-    ZIP:<input type="number" name="ZIP"></input>
-    Home Phone:<input type="phone" name="Home"></input>
-    Mobile Phone:<input type="phone" name="Mobile"></input>
-    Email:<input type="email" name="Email"></input>
+    Field Name:<input type="text" name="FieldName"></input>
+    Acres:<input type="number" name="Acres"></input>
+    County:<input type="text"name="County"></input>
+    Township:<input type="text" name="Township"></input>
+    Section:<input type="text" name="Section"></input>
+    Quarter:<input type="text" name="Quarter"></input>
+    Tillage:<input type="text" name="Tillage"></input>
+    Planting Date:<input type="number" name="PlantingDate"></input>
+    2019 Crop:<input type="text" name="LastYearCrop"></input>
+    How Many Years Corn:<input type="text" name="YearsCorn"></input>
+    Irrigated:<input type="text" name="Irrigated"></input>
+    Rotational:<input type="text" name="Rotational"></input>
+    Crop Year:<input type="number" name="Crop Year"></input>
+    Cover Crop:<input type="text" name="CoverCrop"></input>
+    Date Seeded:<input type="text" name="DateSeeded"></input>
+    How Was It Seeded:<input type="text" name="HowSeeded"></input>
+    Ncredits:<input type="number" name="Ncredits"></input>
+    How Was It Killed:<input type="text" name="HowKilled"></input>
+    Date:<input type="date" name="HowKilled"></input>
     <input type="submit"></input>
   </form>
 <?php
@@ -28,12 +36,12 @@ $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::FETCH_ASSOC);
 }
 catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();
 }
-$sql = 'SELECT ID, FirstName, MI, LastName, CompanyName, MailingAddress, City, State, Zip, HomePhone, MobilePhone, Email FROM grower';
+$sql = 'SELECT * FROM grower';
 $stmt = $connection->prepare($sql);
 $stmt->execute();
 $arr = $stmt->fetchAll(PDO::FETCH_NUM);
 if (count($arr)>0) {
-  echo '<table><tr><th>First Name</th><th>MI</th><th>Last Name</th><th>Company Name</th><th>Mailing Address</th><th>City</th><th>State</th><th>ZIP</th><th>Home Phone</th><th>Mobile Phone</th><th>Email</th></tr>';
+  echo '<table><tr><th>Field Name</th><th>Acres</th><th>County</th><th>Township</th><th>Section</th><th>    </th><th>    </th><th>    </th><th>    </th><th>    </th><th>    </th></tr>';
 foreach ($arr as $i=>$val) {
   array_push($_SESSION['rowPrimaryID'], $val[0]);
   echo '<tr onclick="edit('.$val[0].')">';

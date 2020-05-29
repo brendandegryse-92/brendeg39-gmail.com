304a0766500323f7ef724d280bb11629b03f3c02
brendandegryse-92 <62355127+brendandegryse-92@users.noreply.github.com>
2020-05-29 15:30:05 -0400

    5/29/20

--- chemicalpriceyears.php
+++ chemicalpriceyears.php
@@ -4,27 +4,24 @@
 <title>Chemical Prices</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
+</div>
   <link rel="stylesheet" href="styles.css">
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-    function graph() {
+        function graph() {
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
         var forms = document.getElementsByTagName("form");
-        var arr = [['Year', 'Price']];
+        var arr = [['Lat', 'Long']];
           for (x = 0; x < (forms.length); x++){
-                arr.push([2008 + x, parseforms[x][2].value]);
+                arr.push([new Date(2008 + x, 1, 1), parseFloat(forms[x][2].value)]);
               }
-        console.log(arr);
+        //console.log(arr);
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable(arr);
         /**var data = google.visualization.arrayToDataTable([
-          ['Year', 'Price'],
-          ['2004',  1000],
-          ['2005',  1170],
-          ['2006',  660],
-          ['2007',  1030]
+          ['Year', 'Price']
         ]);**/
 
         var options = {
@@ -39,7 +36,7 @@
       }
     </script>
 </head>
-<body>
+<body><button onclick="graph()">Graph</button>
   <div include="head.html"></div>
   <div style="overflow: auto;">
   <table>
--- manure.php
+++ manure.php
@@ -1,6 +1,11 @@
 <html>
 <head>
   <link rel="stylesheet" href="DataInputPage.css">
+  <style>
+    #Add {
+      display: none;
+    }
+  </style>
 </head>
 <body><h1><?php
 session_start();
@@ -22,7 +27,47 @@ $stmt->execute([$_COOKIE['PrimeIDField'], $_SESSION['ID']]);
 $arr = $stmt->fetchAll(PDO::FETCH_NUM);
 echo $arr[0][0];
 ?></h1>
-  <a href="other.php">Grower</a> <a href="otherfield.php">Field</a> <a href="manure.php">Manure</a> <a href="fertapps.php">Fertilizer Applications</a><div class="newspaper">
+  <a href="other.php">Grower</a> <a href="otherfield.php">Field</a> <a href="manure.php">Manure</a> <a href="fertapps.php">Fertilizer Applications</a><?php
+  session_start();
+  $server = "localhost";
+  $uname = "client";
+  $pword = "Pass";
+  try {
+  $connection = new PDO("mysql:host=$server;dbname=fieldreports",$uname,$pword);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::FETCH_ASSOC);
+  }
+  catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();
+  }
+  $sql = 'SELECT * FROM manure WHERE FieldID = ? AND UserID = ?';
+  $stmt = $connection->prepare($sql);
+  $stmt->execute([$_COOKIE['PrimeIDField'], $_SESSION['ID']]);
+  $arr = $stmt->fetchAll(PDO::FETCH_NUM);
+  if (count($arr)>0) {
+    echo '<table><tr><th>Manure</th><th>AppType</th><th>Time</th><th>Availability</th><th>AppTiming</th><th>AmountPerAcre</th><th>Solid/Liquid</th><th>NPK</th></tr>';
+  foreach ($arr as $i=>$val) {
+    echo '<tr onclick="edit('.$val[0].')">';
+    foreach ($val as $key => $value) {
+      if ($key > 1 && $key != 8) {
+      echo '<td>'.$value.'</td>';
+    }
+    elseif ($key == 8) {
+      if ($value == 0) {echo '<td>Solid</td>';}
+      if ($value == 1) {echo '<td>Liquid</td>';}
+    }
+    }
+    echo '</tr>';
+  }
+  echo '</table>';
+  }
+  $_POST['NPK'];
+    if ($_POST['AppType'] != "") {
+    $sql = 'INSERT INTO manure (FieldID, Manure,	AppType,	Time,	Availability,	AppTiming,	AmountPerAcre, StateOfMatter, NPK, UserID) VALUES (?,?,?,?,?,?,?,?,?,?)';
+    $stmt = $connection->prepare($sql);
+    $stmt->execute([$_COOKIE['PrimeIDField'], $_POST['Manure'], $_POST['AppType'], $_POST['Time'], $_POST['Availability'], $_POST['AppTiming'], $_POST['AmountPerAcre'], $_POST['StateOfMatter'], $_POST['NPK'], $_SESSION['ID']]);
+    $_POST['AppType'] = "";
+    header("Location: manure.php");
+    }
+  ?><button onclick="toggle()">Add Manure</button><div id="Add" class="newspaper">
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     Manure:<input onclick="stop('Swine')" type="radio" id="Manure1" name="Manure1" value="Swine"><label for="Manure1">Swine</label></input><input onclick="stop('Beef')" type="radio" id="Manure2" name="Manure1" value="Beef"><label for="Manure2">Beef</label></input><input onclick="stop('Dairy')" type="radio" id="Manure3" name="Manure1" value="Dairy"><label for="Manure3">Dairy</label></input><input onclick="stop('Layer')" type="radio" id="Manure4" name="Manure1" value="Layer"><label for="Manure4">Layer</label></input><input onclick="stop('Broiler')" type="radio" id="Manure5" name="Manure1" value="Broiler"><label for="Manure5">Broiler</label></input><input onclick="stop('Turkey')" type="radio" id="Manure6" name="Manure1" value="Turkey"><label for="Manure6">Turkey</label></input><input type="radio" id="Manure7" onclick="stop('Layer Pullet')" name="Manure1" value="Layer Pullet"><label for="Manure7">Layer Pullet</label></input>
     <input type="text" id="Manure" name="Manure"></input>
@@ -35,46 +80,7 @@ echo $arr[0][0];
     NPK:<input type="text" pattern="\d{1,2}%\d{1,2}%\d{1,2}%" placeholder="--%--%--%" name="NPK"></input>
     <input type="submit"></input>
   </form></div>
-<?php
-session_start();
-$server = "localhost";
-$uname = "client";
-$pword = "Pass";
-try {
-$connection = new PDO("mysql:host=$server;dbname=fieldreports",$uname,$pword);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::FETCH_ASSOC);
-}
-catch (PDOException $e){echo "failed to connect to database, " . $e->getMessage();
-}
-$sql = 'SELECT * FROM manure WHERE FieldID = ? AND UserID = ?';
-$stmt = $connection->prepare($sql);
-$stmt->execute([$_COOKIE['PrimeIDField'], $_SESSION['ID']]);
-$arr = $stmt->fetchAll(PDO::FETCH_NUM);
-if (count($arr)>0) {
-  echo '<table><tr><th>Manure</th><th>AppType</th><th>Time</th><th>Availability</th><th>AppTiming</th><th>AmountPerAcre</th><th>Solid/Liquid</th><th>NPK</th></tr>';
-foreach ($arr as $i=>$val) {
-  echo '<tr onclick="edit('.$val[0].')">';
-  foreach ($val as $key => $value) {
-    if ($key > 1 && $key != 8) {
-    echo '<td>'.$value.'</td>';
-  }
-  elseif ($key == 8) {
-    if ($value == 0) {echo '<td>Solid</td>';}
-    if ($value == 1) {echo '<td>Liquid</td>';}
-  }
-  }
-  echo '</tr>';
-}
-}
-$_POST['NPK'];
-  if ($_POST['AppType'] != "") {
-  $sql = 'INSERT INTO manure (FieldID, Manure,	AppType,	Time,	Availability,	AppTiming,	AmountPerAcre, StateOfMatter, NPK, UserID) VALUES (?,?,?,?,?,?,?,?,?,?)';
-  $stmt = $connection->prepare($sql);
-  $stmt->execute([$_COOKIE['PrimeIDField'], $_POST['Manure'], $_POST['AppType'], $_POST['Time'], $_POST['Availability'], $_POST['AppTiming'], $_POST['AmountPerAcre'], $_POST['StateOfMatter'], $_POST['NPK'], $_SESSION['ID']]);
-  $_POST['AppType'] = "";
-  header("Location: manure.php");
-  }
-?>
+
 <script>
 function stop(y) {
   var x = document.getElementById("Manure");
@@ -84,6 +90,15 @@ function edit(x) {
   document.cookie="PrimeIDManure=" + x;
   location.href = "editManure.php";
 }
+function toggle() {
+  var x = document.getElementById("Add");
+    if(x.style.display=="inline-block") {
+      x.style.display="none";
+    }
+    else if(x.style.display=="none" || x.style.display=="") {
+      x.style.display="inline-block";
+    }
+  }
 </script>
 </body>
 </html>
